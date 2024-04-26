8ecedafcd99e4512edb2a61793328458a78ff794
Elan <54599408+ElanMedoff@users.noreply.github.com>
2024-04-26 17:06:09 -0400

    fix: add consumer fns to dep array with refs, update readme

--- src/index.ts
+++ src/index.ts
@@ -32,11 +32,11 @@ interface Options<TVal> {
    */
   onError?: (error: unknown) => void;
 }
-// to be deprecated next major version
+// TODO: deprecate next major version
 type UseSearchParamOptions<TVal> = Options<TVal>;
 
 type BuildOptions = Pick<Options<unknown>, "sanitize" | "parse" | "onError">;
-// to be deprecated next major version
+// TODO: deprecate next major version
 type BuildUseSearchParamOptions = BuildOptions;
 
 function maybeGetSearchParam<TVal>({
@@ -54,7 +54,7 @@ function maybeGetSearchParam<TVal>({
   validate: Options<TVal>["validate"];
   buildOnError: Options<TVal>["onError"];
   localOnError: Options<TVal>["onError"];
-  // Required because `parse` has a default value
+  // required because parse has a defualt value
   parse: Required<Options<TVal>>["parse"];
 }) {
   try {
@@ -145,23 +145,37 @@ function buildUseSearchParam(buildOptions: BuildOptions = {}) {
      */
     hookOptions: Options<TVal> = {},
   ) {
-    const parse =
+    const parseOption =
       hookOptions.parse ??
       (buildOptions.parse as Options<TVal>["parse"]) ??
       (defaultParse as Required<Options<TVal>>["parse"]);
-    const sanitize = hookOptions.sanitize ?? buildOptions.sanitize;
-    const { validate, serverSideSearchParams } = hookOptions;
+    const sanitizeOption = hookOptions.sanitize ?? buildOptions.sanitize;
+    const { serverSideSearchParams, validate: validateOption } = hookOptions;
+
+    const parseRef = React.useRef(parseOption);
+    const sanitizeRef = React.useRef(sanitizeOption);
+    const validateRef = React.useRef(validateOption);
+    const buildOnErrorRef = React.useRef(buildOptions.onError);
+    const hookOnErrorRef = React.useRef(hookOptions.onError);
+
+    React.useEffect(() => {
+      parseRef.current = parseOption;
+      sanitizeRef.current = sanitizeOption;
+      validateRef.current = validateOption;
+      buildOnErrorRef.current = buildOptions.onError;
+      hookOnErrorRef.current = hookOptions.onError;
+    });
 
     React.useEffect(() => {
       const onEvent = () => {
         const newSearchParamVal = maybeGetSearchParam({
           searchParamKey,
           serverSideSearchParams,
-          sanitize,
-          parse,
-          validate,
-          buildOnError: buildOptions.onError,
-          localOnError: hookOptions.onError,
+          sanitize: sanitizeRef.current,
+          parse: parseRef.current,
+          validate: validateRef.current,
+          buildOnError: buildOnErrorRef.current,
+          localOnError: hookOnErrorRef.current,
         });
 
         setSearchParamVal(newSearchParamVal);
@@ -177,9 +191,9 @@ function buildUseSearchParam(buildOptions: BuildOptions = {}) {
         maybeGetSearchParam({
           searchParamKey,
           serverSideSearchParams,
-          sanitize,
-          parse,
-          validate,
+          sanitize: sanitizeRef.current,
+          parse: parseRef.current,
+          validate: validateRef.current,
           buildOnError: buildOptions.onError,
           localOnError: hookOptions.onError,
         }),
